bb68b17e79e9d02b22bdf265278d2c65f61ae3a6
NikhilJain8000 <94992408+NikhilJain8000@users.noreply.github.com>
2023-06-09 17:03:11 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages,setup
+from typing import List
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path)as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("/n ","")for req in requirements]
+
+    return requirements
+
+setup(
+	name='DiamondPredictionProject',
+	version='0.0.1',
+	author='Nikhil Jain',
+	author_email='nikhiljain6350@gnmail.com',
+    install_requires=get_requirements('requirements.txt'),
+	packages=find_packages()
+	
+)
